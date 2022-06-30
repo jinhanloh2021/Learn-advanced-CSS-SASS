9f8d3a3b37b1fa1f84c0216917b3b8e3e865f374
unknown <jinhan.loh.2021@scis.smu.edu.sg>
2022-06-30 14:12:43 +0800

    Changes.

--- Trillo/src/components/content/sidebar/Sidebar.tsx
+++ Trillo/src/components/content/sidebar/Sidebar.tsx
@@ -4,5 +4,17 @@ import './sidebar.scss';
 type Props = {};
 
 export default function Sidebar({}: Props) {
-  return <nav className="sidebar">Navigation</nav>;
+  return (
+    <nav className="sidebar">
+      <ul className="side-nav">
+        <li className="side-nav__item">
+          <a href="#" className="side-nav__link">
+            <svg className="side-nav__icon">
+              <use xlinkHref='img/sprite.svg#icon-home'></use>
+            </svg>
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
 }

cf0bf3dd3eeafb609c1f8a7a979ad271c187cbae
Jin_Han <jinhan.loh.2021@scis.smu.edu.sg>
2022-08-15 16:03:59 +0800

    CT-4 Added comments

--- Trillo/src/components/Container.tsx
+++ Trillo/src/components/Container.tsx
@@ -4,6 +4,7 @@ import Header from './header/Header';
 import Content from './content/Content';
 
 // type Props = {}; //interface definition
+//container adds padding to left and right of entire webpage. Also adds background colour.
 
 export default function Container(): JSX.Element {
   return (
